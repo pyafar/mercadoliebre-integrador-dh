b0d1cef1af7c925ce4cd2f4e04c6d70ae24434de
pali <paulayafar.86@gmail.com>
2022-04-20 12:06:40 -0300

    ajustes visuales

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log('Servidor corriendo en puerto 3000')
 })
 
 app.use(express.static(__dirname + '/public')); 
 
 app.get('/', (req, res)=> {
-    res.sendFile(path.join(__dirname, '/views/home.html'))
+    res.sendFile(path.join(__dirname, '/views/index.html'))
 });
 
 app.get('/registro', (req, res)=> {
